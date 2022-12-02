f3ba12e1341718785f6b259d775737fa62f1af7c
AliceVivette <84288272+AliceVivette@users.noreply.github.com>
2022-12-02 09:19:42 +0100

    ljbgv

---
+++ src/Num_Exception.java
@@ -0,0 +1,5 @@
+class Num_Exception extends Exception {   // Exception sur la taille du numéro de sécurité social du client
+    public Num_Exception (String str) {    
+        super(str);  
+    }  
+}     
--- src/Pharmacie.java
+++ src/Pharmacie.java
@@ -33,19 +33,38 @@ public class Pharmacie {
 
     }
 
-    public void achat_medoc(String ref, int qt, ArrayList liste_achat){
+    public void achat_medoc(String ref, int qt, ArrayList<Medicament> liste_achat){
 
+    }
+
+
+    static void check_num (String n) throws Num_Exception{ 
+        if(n.length() < 13){  
+            throw new Num_Exception("Erreur sur le numéro de sécurité social, sa longueur est inférieur à 13 ");   
+            // on throw une nouvelle exception si length n < 13
+        }  
+        else {     
+            // sinon tout va bien
+        }   
     }
 
     public static void main(String[] args) throws Exception {
         ArrayList<Client> liste_clients=new ArrayList<Client>();;
 
-        Client c1 = new Client("ROY", "Victorine", "2020778158258", "Chateau de Versailles", "06 62 03 12 96");
-        Client c2 = new Client("VIVET", "Alice", "1234567898765", "Chateau de Versailles", "06 62 03 12 96");
         Medicament m1 = new Medicament("DLP500", "doliprane 500mg", "antalgique", "2,86 €", 1);
 
-        liste_clients.add(c1); //+message de confirmation 
+        Client c1 = new Client("ROY", "Victorine", "2020778158258", "Chateau de Versailles", "06 62 03 12 96");
+        Client c2 = new Client("VIVET", "Alice", "1234567898765", "Chateau de Versailles", "06 62 03 12 96");
+
+        // Verifier Exception sur numero sécu
+        try {     
+        check_num(c2.numero_secu);  
         liste_clients.add(c2);
+        }  
+        catch (Num_Exception ex){     
+        System.out.println(ex);  
+        }
+        
 
         recherche_client(liste_clients, "1234567898765");
 
